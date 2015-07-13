7bc74c58efc3d1343795720d6629013c4bb00f24
maddyonline <madhavjha@gmail.com>
2015-07-12 23:52:26 -0700

    more

--- erg.go
+++ erg.go
@@ -25,6 +25,41 @@ export default DS.ActiveModelAdapter.extend({
 	namespace: 'api'
 });`
 
+const FORM = `
+<form {{action "save" on="submit"}}> <p>
+<label>First Name:
+{{input value=model.firstName}}
+    </label>
+  </p>
+<p>
+<label>Last Name:
+      {{input value=model.lastName }}
+    </label>
+  </p>
+ <p> <label>Email:
+      {{input value=model.email}}
+    </label>
+  </p>
+<p> <label>Twitter
+      {{input value=model.twitter}}
+    </label>
+  </p>
+<input type="submit" value="Save"/>
+<button {{action "cancel"}}>Cancel</button> </form>
+`
+
+const INDEX = `
+<h1>Friends Index</h1>
+{{! The context here is the controller}} <h2>Total friends: {{model.length}}</h2>
+<ul>
+{{#each friend in model}}
+<li>{{friend.id}} - {{friend.firstName}} {{friend.lastName}}</li>
+{{/each}}
+</ul>
+`
+
+const ADD_NEW = `<h1>Adding New Friend</h1> {{partial "friends/form"}}`
+
 const script = `
 ember new borrowers && \
 ember g resource friends firstName:string lastName:string email:string twitter:string totalArticles:number 
@@ -50,6 +85,58 @@ func updateAdapter(app *EmberApp) pipe.Pipe {
 	return p
 }
 
+func createForm(app *EmberApp) pipe.Pipe {
+	p := pipe.Script(
+		pipe.Exec("ember", "g", "template", fmt.Sprintf("%s/-form", app.Resource.Name)),
+		pipe.TeeLine(
+			pipe.Read(strings.NewReader(FORM)),
+			pipe.WriteFile("app/templates/friends/-form.hbs", 0644),
+		),
+	)
+	return p
+}
+
+func createIndex(app *EmberApp) pipe.Pipe {
+	p := pipe.Script(
+		pipe.Exec("ember", "g", "route", fmt.Sprintf("%s/index", app.Resource.Name)),
+		pipe.TeeLine(
+			pipe.Read(strings.NewReader(INDEX)),
+			pipe.WriteFile("app/templates/friends/index.hbs", 0644),
+		),
+	)
+	return p
+}
+
+func createNew(app *EmberApp) pipe.Pipe {
+	p := pipe.Script(
+		pipe.Exec("ember", "g", "route", fmt.Sprintf("%s/new", app.Resource.Name)),
+		pipe.Exec("ember", "g", "controller", fmt.Sprintf("%s/new", app.Resource.Name)),
+		pipe.TeeLine(
+			pipe.Read(strings.NewReader(ADD_NEW)),
+			pipe.WriteFile("app/templates/friends/new.hbs", 0644),
+		),
+	)
+	return p
+}
+
+func createBasic(app *EmberApp) pipe.Pipe {
+	p := pipe.Script(
+		createForm(app),
+		createIndex(app),
+		//pipe.Exec("ember", "g", "route", fmt.Sprintf("%s/new", app.Resource.Name)),
+		//pipe.Exec("ember", "g", "route", 
+		//	fmt.Sprintf("%s/show", app.Resource.Name), 
+		//	fmt.Sprintf("--path=:%s_id", strings.TrimSuffix(app.Resource.Name, "s")),),
+		//pipe.Exec("ember", "g", "route", 
+		//	fmt.Sprintf("%s/edit", app.Resource.Name), 
+		//	fmt.Sprintf("--path=:%s_id/edit", strings.TrimSuffix(app.Resource.Name, "s")),),
+		//pipe.Exec("ember", "g", "controller", fmt.Sprintf("%s/base", app.Resource.Name)),
+		//pipe.Exec("ember", "g", "controller", fmt.Sprintf("%s/new", app.Resource.Name)),
+		//pipe.Exec("ember", "g", "controller", fmt.Sprintf("%s/edit", app.Resource.Name)),
+	)
+	return p
+}
+
 func createEmberApp(app *EmberApp) pipe.Pipe {
 	genResourceArgs := append([]string{"g", "resource", app.Resource.Name}, app.Resource.Properties...)
 	p := pipe.Script(
@@ -57,6 +144,7 @@ func createEmberApp(app *EmberApp) pipe.Pipe {
 		pipe.ChDir(app.Name),
 		pipe.Exec("ember", genResourceArgs...),
 		updateAdapter(app),
+		createBasic(app),
 	)
 	return p
 }
